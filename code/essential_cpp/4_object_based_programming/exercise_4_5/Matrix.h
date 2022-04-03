8453b4ac203c04b1fb887fddce42fb6c87e9c58c
Xingxin HE <xingxin.he@mail.polimi.it>
2022-04-03 20:45:17 +0800

    Exercise 4.5 Finished!!✔

--- code/essential_cpp/4_object_based_programming/exercise_4_5/Matrix.h
+++ code/essential_cpp/4_object_based_programming/exercise_4_5/Matrix.h
@@ -1,16 +1,154 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef float elemType;
+
 class Matrix
 {
+    
 private:
-    /* data */
+    elemType _matrix[4][4];
+    
 public:
-    Matrix(/* args */);
+    Matrix
+    (
+            elemType=0.,elemType=0.,elemType=0.,elemType=0.,
+            elemType=0.,elemType=0.,elemType=0.,elemType=0.,
+            elemType=0.,elemType=0.,elemType=0.,elemType=0.,
+            elemType=0.,elemType=0.,elemType=0.,elemType=0. 
+    );
+    Matrix(elemType*);    
     ~Matrix();
+
+    friend Matrix operator+( const Matrix&, const Matrix& );
+    friend Matrix operator*( const Matrix&, const Matrix& );
+    void operator+=(const Matrix&);
+    elemType& operator()(int row, int col) {return _matrix[row][col];}
+    elemType operator()(int row, int col) const {return _matrix[row][col];}
+
+    int row() const {return 4;}
+    int col() const {return 4;}
+    elemType get_elem(int n_row, int n_col) const;
+    bool validate_index(int n_row, int n_col) const;
+    ostream& print(ostream &os) const;
 };
 
-Matrix::Matrix(/* args */)
+Matrix::Matrix
+(
+    elemType m00, elemType m01, elemType m02, elemType m03,
+    elemType m10, elemType m11, elemType m12, elemType m13,
+    elemType m20, elemType m21, elemType m22, elemType m23,
+    elemType m30, elemType m31, elemType m32, elemType m33
+)
 {
+    _matrix[0][0]=m00; _matrix[0][1]=m01; _matrix[0][2]=m02; _matrix[0][3]=m03;
+    _matrix[1][0]=m10; _matrix[1][1]=m11; _matrix[1][2]=m12; _matrix[1][3]=m13;
+    _matrix[2][0]=m20; _matrix[2][1]=m21; _matrix[2][2]=m22; _matrix[2][3]=m23;
+    _matrix[3][0]=m30; _matrix[3][1]=m31; _matrix[3][2]=m32; _matrix[3][3]=m33;
+}
+
+Matrix::Matrix(elemType* array)
+{
+    int cnt = 0;
+    for (int ix = 0; ix < row(); ix++)
+    {
+        for (int iy = 0; iy < col(); iy++)
+        {
+            _matrix[ix][iy] = array[cnt++];
+        }
+    }
 }
 
 Matrix::~Matrix()
 {
+
 }
+
+ostream&
+Matrix::print(ostream &os) const
+{
+    for (int ix = 0; ix < 4; ix++)
+    {
+        for (int iy = 0; iy < 4; iy++)
+        {
+            os << _matrix[ix][iy] << " ";
+        }
+        os << endl;
+    }
+    return os;
+}
+
+inline void
+Matrix::operator+=(const Matrix& m)
+{
+    for (int ix = 0; ix < row(); ix++)
+    {
+        for (int iy = 0; iy < col(); iy++)
+        {
+            this->_matrix[ix][iy] += m._matrix[ix][iy];
+        }
+    }
+}
+
+inline
+Matrix operator*(Matrix& m1, Matrix& m2)
+{
+    Matrix m;
+    for (int ix = 0; ix < m.row(); ix++)
+    {
+        for (int iy = 0; iy < m.col(); iy++)
+        {
+            m(ix, iy) = 0;
+            for (int iz = 0; iz < m.col(); iz++)
+            {
+                 m(ix, iy) += ( m1(ix, iz) * m2(iz, iy) );
+            }
+        }
+    }
+    return m;
+}
+
+Matrix operator+(Matrix& m1, Matrix& m2)
+{
+    Matrix result(m1);
+    result += m2;
+    return result;
+}
+
+inline
+ostream& operator<< (ostream &os, const Matrix &rhs)
+{
+    return rhs.print(os);
+}
+
+bool
+Matrix::validate_index(int n_row, int n_col) const
+{
+    if (n_row < 0 || n_col <0 || n_row > 3 || n_col > 3)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+elemType
+Matrix::get_elem(int n_row, int n_col) const
+{
+    if (Matrix::validate_index(n_row, n_col))
+    {
+        return _matrix[n_row][n_col];
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+
+
--- code/essential_cpp/4_object_based_programming/exercise_4_5/main.cpp
+++ code/essential_cpp/4_object_based_programming/exercise_4_5/main.cpp
@@ -6,35 +6,73 @@ using namespace std;
 
 int main()
 {
+    // test 1
+    cout << "Test 1:" << endl;
     Matrix m;
     cout << m << endl;
 
-	elemType ar[16]={
-		1., 0., 0., 0., 0., 1., 0., 0.,
-		0., 0., 1., 0., 0., 0., 0., 1. };
+    // test 2
+    cout << "Test 2:" << endl;
+    elemType ar[16]={
+        1., 0., 0., 0., 0., 1., 0., 0.,
+        0., 0., 1., 0., 0., 0., 0., 1. };
+    Matrix identity( ar );
+    cout << identity << endl;
 
-	Matrix identity( ar );
-	cout << identity << endl;
+    // test 3&4
+    cout << "Test 3:" << endl;
+    Matrix m2( identity );
+    m = identity;
+    cout << m2 << endl; 
+    cout << "Test 4:" << endl;
+    cout << m  << endl;
 
-	// guarantee default memberwise copy works correctly
-	Matrix m2( identity );
-	m = identity;
+    // test 5
+    cout << "Test 5:" << endl;
+    elemType ar2[16]={
+        1.3f, 0.4f, 2.6f, 8.2f, 6.2f, 1.7f, 1.3f, 8.3f,
+        4.2f, 7.4f, 2.7f, 1.9f, 6.3f, 8.1f, 5.6f, 6.6f };
+    Matrix m3( ar2 );
+    cout << m3 << endl;
 
-	cout << m2 << endl; cout << m  << endl;
+    // test 6
+    cout << "Test 6:" << endl;
+    Matrix m4 = m3 * identity;
+    cout << m4 << endl;
 
-	elemType ar2[16]={
-		1.3f, 0.4f, 2.6f, 8.2f, 6.2f, 1.7f, 1.3f, 8.3f,
-		4.2f, 7.4f, 2.7f, 1.9f, 6.3f, 8.1f, 5.6f, 6.6f };
-
-	Matrix m3( ar2 );
-	cout << m3 << endl;
-
-	Matrix m4 = m3 * identity;
-	cout << m4 << endl;
-
-	Matrix m5 = m3 + m4;
-	cout << m5 << endl;
-
-	m3 += m4;
-	cout << m3 << endl;
+    // test 7
+    cout << "Test 7:" << endl;
+    Matrix m5 = m3 + m4;
+    cout << m5 << endl;
+    
+    // test 8
+    cout << "Test 8:" << endl;
+    m3 += m4;
+    cout << m3 << endl;
 }
+
+// expected output
+// test 1
+// 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0
+// test 2
+// 1 0 0 0 0 1 0 0
+// 0 0 1 0 0 0 0 1
+// test 3
+// 1 0 0 0 0 1 0 0
+// 0 0 1 0 0 0 0 1
+// test 4
+// 1 0 0 0 0 1 0 0
+// 0 0 1 0 0 0 0 1
+// test 5
+// 1.3 0.4 2.6 8.2 6.2 1.7 1.3 8.3
+// 4.2 7.4 2.7 1.9 6.3 8.1 5.6 6.6
+// test 6
+// 1.3 0.4 2.6 8.2 6.2 1.7 1.3 8.3
+// 4.2 7.4 2.7 1.9 6.3 8.1 5.6 6.6
+// test 7
+// 2.6 0.8 5.2 16.4 12.4 3.4 2.6 16.6
+// 8.4 14.8 5.4 3.8 12.6 16.2 11.2 13.2
+// test 8
+// 2.6 0.8 5.2 16.4 12.4 3.4 2.6 16.6
+// 8.4 14.8 5.4 3.8 12.6 16.2 11.2 13.2
