2799ddc1880ef4aad90a93cbb6e819ce4ea07803
Xingxin HE <xingxin.he@mail.polimi.it>
2022-04-11 00:01:38 +0800

    assignment 1 - OnlyConnect✔

--- code/programming_abstraction_in_cpp/lecture/2021_autumn/Assignment_1/src/OnlyConnect.cpp
+++ code/programming_abstraction_in_cpp/lecture/2021_autumn/Assignment_1/src/OnlyConnect.cpp
@@ -6,14 +6,54 @@
  */
 #include "OnlyConnect.h"
 #include "GUI/SimpleTest.h"
+#include "strlib.h"
 using namespace std;
 
+
+static const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+static const string vowel = "aeiouy";
+
+bool isVowel(char c)
+{
+    if((vowel.find(c) != string::npos) || (toUpperCase(vowel).find(c)!=string::npos))
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isAlphabet(char c)
+{
+    if((alphabet.find(c) != string::npos) || (toUpperCase(alphabet).find(c)!=string::npos))
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 string onlyConnectize(string phrase) {
-    /* TODO: The next few lines just exist to make sure you don't get compiler warning messages
-     * when this function isn't implemented. Delete these lines, then implement this function.
-     */
-    (void) phrase;
-    return "";
+
+    int length = phrase.length();
+    for(int i = length - 1; i >= 0; i--)
+    {
+        char c = phrase[i];
+        if(isAlphabet(c) && !isVowel(c))
+        {
+
+        }
+        else
+        {
+            phrase.erase(i, 1);
+        }
+    }
+
+    return toUpperCase(phrase);
 }
 
 
@@ -39,20 +79,18 @@ PROVIDED_TEST("Handles single-character inputs.") {
     EXPECT_EQUAL(onlyConnectize("Q"), "Q");
 }
 
-/* TODO: You will need to add your own tests into this suite of test cases. Think about the sorts
- * of inputs we tested here, and, importantly, what sorts of inputs we *didn't* test here. Some
- * general rules of testing:
- *
- *    1. Try extreme cases. What are some very large cases to check? What are some very small cases?
- *
- *    2. Be diverse. There are a lot of possible inputs out there. Make sure you have tests that account
- *       for cases that aren't just variations of one another.
- *
- *    3. Be sneaky. Don't just try standard inputs. Try weird ones that you wouldn't expect anyone to
- *       actually enter, but which are still perfectly legal.
- *
- * Happy testing!
- */
+PROVIDED_TEST("Serenity Prayer") {
+    EXPECT_EQUAL(onlyConnectize("God, grant me the serenity."), "GDGRNTMTHSRNT");
+    EXPECT_EQUAL(onlyConnectize("Serenity Prayer"), "SRNTPRR");
+    EXPECT_EQUAL(onlyConnectize(" Reinhold Niebuhr"), "RNHLDNBHR");
+}
+
+PROVIDED_TEST("Chinese Character") {
+    EXPECT_EQUAL(onlyConnectize("记者提问：领导真的有走访过基层么？"), "");
+    EXPECT_EQUAL(onlyConnectize("记者提问：因疫情身患重病不能及时医治的病人还有失业还不上贷款自杀的人加起来比因omicron病死的人多吗？"), "MCRN");
+    EXPECT_EQUAL(onlyConnectize("记者提问：拉高gdp数字，继续吸纳二三线的青壮年来打工。"), "GDP");
+}
+
 
 
 
--- code/programming_abstraction_in_cpp/lecture/2021_autumn/Assignment_1/src/PlayingFair.cpp
+++ code/programming_abstraction_in_cpp/lecture/2021_autumn/Assignment_1/src/PlayingFair.cpp
@@ -62,20 +62,6 @@ PROVIDED_TEST("Triggers error on negative inputs.") {
     EXPECT_ERROR(bSequenceOfOrder(-137));
 }
 
-/* TODO: You will need to add your own tests into this suite of test cases. Think about the sorts
- * of inputs we tested here, and, importantly, what sorts of inputs we *didn't* test here. Some
- * general rules of testing:
- *
- *    1. Try extreme cases. What are some very large cases to check? What are some very small cases?
- *
- *    2. Be diverse. There are a lot of possible inputs out there. Make sure you have tests that account
- *       for cases that aren't just variations of one another.
- *
- *    3. Be sneaky. Don't just try standard inputs. Try weird ones that you wouldn't expect anyone to
- *       actually enter, but which are still perfectly legal.
- *
- * Happy testing!
- */
 
 
 
