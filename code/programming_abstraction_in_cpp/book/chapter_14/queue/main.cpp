568cb877c9d1251f6adcfa33cdd28a25763690ef
XingxinHE <xingxin.he@mail.polimi.it>
2022-05-30 10:52:05 +0800

    template queue✅

--- code/programming_abstraction_in_cpp/book/chapter_14/queue/main.cpp
+++ code/programming_abstraction_in_cpp/book/chapter_14/queue/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cassert>
+#include <string>
 #include "queue.h"
 
 using namespace std;
 
 int main()
 {
+    /*--------------------ASSERT Integer Queue--------------------*/
     Queue<int> intQ;
     assert(("The queue is empty.", intQ.size() == 0));
     assert(("The queue is empty.", intQ.isEmpty() == true));
@@ -22,6 +24,7 @@ int main()
     assert(("3rd dequeue is 28.", intQ.dequeue() == 28));
     assert(("The queue is empty.", intQ.isEmpty() == true));
 
+    /*--------------------ASSERT Char Queue--------------------*/
     Queue<char> charQ;
     for(int i = 0; i< 26; i++)
     {
@@ -32,6 +35,45 @@ int main()
     assert(("The first one is 'A'.", charQ.peek() == 'A'));
     charQ.clear();
     assert(("The queue is empty.", charQ.isEmpty() == true));
+
+    /*--------------------ASSERT String Queue--------------------*/
+    Queue<string> stringQ;
+    stringQ.enqueue(string("SEX"));
+    stringQ.enqueue(string("WORLD"));
+    stringQ.enqueue(string("peace"));
+    Queue<string> stringQcopy1(stringQ);
+    Queue<string> stringQcopy2 = stringQ;
+    assert(("Dequeue 1st is SEX.", stringQcopy1.dequeue() == string("SEX")));
+    assert(("Dequeue 2nd is SEX.", stringQcopy1.dequeue() == string("WORLD")));
+    assert(("Dequeue 3rd is SEX.", stringQcopy1.dequeue() == string("peace")));
+    assert(("Dequeue 1st is SEX.", stringQcopy2.dequeue() == string("SEX")));
+    assert(("Dequeue 2nd is SEX.", stringQcopy2.dequeue() == string("WORLD")));
+    assert(("Dequeue 3rd is SEX.", stringQcopy2.dequeue() == string("peace")));
+
+    /*--------------------PLAYGROUND double Queue--------------------*/
+    cout << "cout playground..." << endl;
+    Queue<double> doubleQ;
+    cout << doubleQ.isEmpty() << endl;
+    cout << doubleQ.size() << endl;
+    doubleQ.enqueue(3.14);
+    doubleQ.enqueue(-3131.4332);
+    doubleQ.enqueue(12.13);
+    cout << doubleQ.isEmpty() << endl;
+    cout << doubleQ.size() << endl;
+    cout << doubleQ.peek() << endl;
+    cout << doubleQ.dequeue() << endl;
+    cout << doubleQ.dequeue() << endl;
+    cout << doubleQ.dequeue() << endl;
+    cout << doubleQ.isEmpty() << endl;
+    cout << doubleQ.size() << endl;
+    doubleQ.enqueue(983213.1231);
+    doubleQ.enqueue(-67.567);
+    doubleQ.enqueue(-3.237);
+    cout << doubleQ.isEmpty() << endl;
+    cout << doubleQ.size() << endl;
+    doubleQ.clear();
+    cout << doubleQ.isEmpty() << endl;
+    cout << doubleQ.size() << endl;
 
     return 0;
 }
--- code/programming_abstraction_in_cpp/book/chapter_14/queue/queue.h
+++ code/programming_abstraction_in_cpp/book/chapter_14/queue/queue.h
@@ -1,4 +1,3 @@
-#include <stdexcept>
 /*
 * File: queue.h
 * -------------
@@ -7,6 +6,8 @@
 */
 #ifndef _queue_h
 #define _queue_h
+
+#include <stdexcept>
 /*
 * Class: Queue<ValueType>
 * -----------------------
@@ -28,6 +29,16 @@ public:
     * Initializes a new empty queue.
     */
     Queue<ValueType>();
+
+
+
+    /*
+    * Constructor: Queue
+    * Usage: Queue<ValueType> dst(src);
+    * ---------------------------------
+    * Initializes a new empty queue.
+    */
+    Queue<ValueType>(const Queue<ValueType> &src);
 
 
 
@@ -100,6 +111,17 @@ public:
     ValueType peek() const;
 
 
+    /*
+    * Operator: =
+    * Usage: Queue<ValueType> q1;
+    *        Queue<ValueType> q2;
+    *        q1 = q2;
+    * ---------------------------
+    * Overload the '== operator.
+    */
+    Queue<ValueType> & operator=(const Queue<ValueType> &src);
+
+
 
 /* Private Section */
 private:
@@ -115,32 +137,81 @@ private:
         {
         }
     };
-    
+
+/* Private Members */    
     int m_count;
     LinkNode *m_startCell;
     LinkNode *m_endCell;
 
+/* Private Functions */
+void deepCopy(const Queue<ValueType> &src);
+
 };
 
 
+/*
+* Implementation notes: Queue data structure
+* ------------------------------------------
+* The list-based queue uses a linked list to store the elements
+* of the queue. To ensure that adding a new element to the tail
+* of the queue is fast, the data structure maintains a pointer to
+* the last cell in the queue as well as the first. If the queue is
+* empty, both the head pointer and the tail pointer are set to NULL.
+*
+*/
+
+
+
+/*
+* Implementation notes: Queue constructor
+* ---------------------------------------
+* The constructor creates an empty linked list and sets the count to 0.
+*/
 template<typename ValueType>
 Queue<ValueType>::Queue()
                 :m_count(0), m_startCell(nullptr), m_endCell(nullptr)
 {
 }
 
+
+
+/*
+* Implementation notes: copy operator
+* -----------------------------------
+* These methods follow the standard template, leaving the work to deepCopy.
+*/
+template<typename ValueType>
+Queue<ValueType>::Queue(const Queue<ValueType> &src)
+                :m_count(0), m_startCell(nullptr), m_endCell(nullptr)
+{
+    deepCopy(src);
+}
+
+
+
+/*
+* Implementation notes: ~Queue destructor
+* ---------------------------------------
+* The destructor frees any heap memory allocated by the queue.
+*/
 template<typename ValueType>
 Queue<ValueType>::~Queue()
 {
     clear();
 }
 
+
+
+/*
+* Implementation notes: size, isEmpty, clear
+* ------------------------------------------
+* The implementation maintains count so that size remains O(1).
+*/
 template<typename ValueType>
 int Queue<ValueType>::size() const
 {
     return m_count;
 }
-
 
 template<typename ValueType>
 bool Queue<ValueType>::isEmpty() const
@@ -151,15 +222,21 @@ bool Queue<ValueType>::isEmpty() const
 template<typename ValueType>
 void Queue<ValueType>::clear()
 {
-    while (m_startCell != nullptr)
+    while (m_count > 0)
     {
-        LinkNode *node = m_startCell;
-        m_startCell = m_startCell->next;
-        delete node;
+        this->dequeue();
     }
-    m_endCell = nullptr;
 }
 
+
+
+/*
+* Implementation notes: enqueue
+* -----------------------------
+* This method allocates a new list cell and chains it in at the tail of
+* the queue. If the queue is currently empty, the new cell must also
+* become the head pointer in the queue.
+*/
 template<typename ValueType>
 void Queue<ValueType>::enqueue(ValueType value)
 {
@@ -177,10 +254,20 @@ void Queue<ValueType>::enqueue(ValueType value)
     return;
 }
 
+
+
+/*
+* Implementation notes: dequeue, peek
+* -----------------------------------
+* These methods must check for an empty queue and report an error if
+* there is no first element. The dequeue method must also check for
+* the case in which the queue becomes empty and set both the head
+* and tail pointers to NULL.
+*/
 template<typename ValueType>
 ValueType Queue<ValueType>::dequeue()
 {
-    if(m_count == 0)
+    if(isEmpty())
     {
         throw std::invalid_argument("Try to dequeue from an empty queue.");
     }
@@ -189,11 +276,11 @@ ValueType Queue<ValueType>::dequeue()
     ValueType result = m_startCell->value;
     LinkNode *node = m_startCell;
     m_startCell = m_startCell->next;
-    delete node;
-    if (m_count == 0)
+    if (m_startCell == nullptr)
     {
         m_endCell = nullptr;
     }
+    delete node;
     
     return result;
 }
@@ -201,13 +288,48 @@ ValueType Queue<ValueType>::dequeue()
 template<typename ValueType>
 ValueType Queue<ValueType>::peek() const
 {
-    if(m_count == 0)
+    if(isEmpty())
     {
         throw std::invalid_argument("Try to peek from an empty queue.");
     }
-    ValueType result = m_startCell->value;
     
-    return result;
+    return m_startCell->value;
+}
+
+
+
+/*
+* Implementation notes: assignment operator
+* -----------------------------------------
+* These methods follow the standard template, leaving the work to deepCopy.
+*/
+template<typename ValueType>
+Queue<ValueType> & Queue<ValueType>::operator=(const Queue<ValueType> &src)
+{
+    if(this != &src)
+    {
+        clear();
+        deepCopy(src);
+    }
+    return *this;
+}
+
+
+
+/*
+* Implementation notes: deepCopy
+* ------------------------------
+* This function copies the data from the src parameter into the current obejct.
+* This implementation simply walks down the linked list in the source object
+* and enqueue each value in the destination.
+*/
+template<typename ValueType>
+void Queue<ValueType>::deepCopy(const Queue<ValueType> &src)
+{
+    for(LinkNode *start = src.m_startCell; start != nullptr; start = start->next)
+    {
+        this->enqueue(start->value);
+    }
 }
 
 #endif
